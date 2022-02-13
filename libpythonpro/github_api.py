1e481b05a6fb254738f457c19e8a9cc388e14a1c
Fabio Silva <fabiocarini2003@gmail.com>
2022-02-13 01:07:09 -0300

    Instalacao pip e exercicio avatar

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,16 @@
+import requests
+
+def buscar_avatar (usuario):
+    """"
+    Busca o avatar de um usuario no Github
+
+    :param usuario: str com o nome de usuario no gitthub
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('fabio1008'))
+
