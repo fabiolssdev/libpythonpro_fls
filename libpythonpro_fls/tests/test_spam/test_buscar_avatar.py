d127da1921d617c2a92af650a7bc6dfad88f9e70
Fabio Silva <fabiocarini2003@gmail.com>
2022-02-19 20:31:52 -0300

    Duvida tirada sobre o erro.

--- libpythonpro_fls/tests/test_spam/test_buscar_avatar.py
+++ libpythonpro_fls/tests/test_spam/test_buscar_avatar.py
@@ -9,6 +9,12 @@ def test_buscar_avatar():
         'login': 'fabio1008', 'id': 83844802,
         'avatar_url': 'https://avatars.githubusercontent.com/u/83844802?v=4',
     }
+    get_original=github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
     url = github_api.buscar_avatar('fabio1008')
+    assert 'https://avatars.githubusercontent.com/u/83844802?v=4' == url
+    github_api.requests.get = get_original
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('fabio')
     assert 'https://avatars.githubusercontent.com/u/83844802?v=4' == url
