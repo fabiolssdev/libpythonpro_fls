2f2a1167e9d948e2c0420b10d79b73c5ae1390c2
Fabio Silva <fabiocarini2003@gmail.com>
2022-02-20 18:30:29 -0300

    Biblioteca PytestMock.

--- libpythonpro_fls/tests/test_spam/test_github_api.py
+++ libpythonpro_fls/tests/test_spam/test_github_api.py
@@ -4,18 +4,19 @@ import pytest
 
 from libpythonpro_fls import github_api
 
+
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/83844802?v=4'
     resp_mock.json.return_value = {
         'login': 'fabio1008', 'id': 83844802,
         'avatar_url': url,
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonpro_fls.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
 
 def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('fabio1008')
