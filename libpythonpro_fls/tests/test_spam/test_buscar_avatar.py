e494bc45836688aaa8a0b31de8b76fafd24532f0
Fabio Silva <fabiocarini2003@gmail.com>
2022-02-20 18:05:50 -0300

    isolamento de Imports

--- libpythonpro_fls/tests/test_spam/test_buscar_avatar.py
+++ libpythonpro_fls/tests/test_spam/test_buscar_avatar.py
@@ -1,19 +1,26 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro_fls import github_api
 
-
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/83844802?v=4'
     resp_mock.json.return_value = {
         'login': 'fabio1008', 'id': 83844802,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/83844802?v=4',
+        'avatar_url': url,
     }
-    get_original=github_api.requests.get
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
-    url = github_api.buscar_avatar('fabio1008')
-    assert 'https://avatars.githubusercontent.com/u/83844802?v=4' == url
+    yield url
     github_api.requests.get = get_original
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('fabio1008')
+    assert avatar_url == url
+
 
 def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('fabio1008')
